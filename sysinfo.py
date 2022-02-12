114f5cdb2a9f5f7753be025afe950c950b6adf24
ASA264 <84116335+ASA264@users.noreply.github.com>
2022-02-12 16:15:36 +0530

    Create sysinfo.py

---
+++ sysinfo.py
@@ -0,0 +1,55 @@
+print(""" ______   ______ _____ _____ __  __
+/ ___\ \ / / ___|_   _| ____|  \/  |
+\___\ \ V /\___ \ | | |  _| | |\/| |
+ ___) || |  ___) || | | |___| |  | |
+|____/ |_| |____/ |_| |_____|_|  |_|
+
+ ___ _   _ _____ ___  ____  __  __    _  _____ ___ ___  _   _
+|_ _| \ | |  ___/ _ \|  _ \|  \/  |  / \|_   _|_ _/ _ \| \ | |
+ | ||  \| | |_ | | | | |_) | |\/| | / _ \ | |  | | | | |  \| |
+ | || |\  |  _|| |_| |  _ <| |  | |/ ___ \| |  | | |_| | |\  |
+|___|_| \_|_|   \___/|_| \_\_|  |_/_/   \_\_| |___\___/|_| \_|""")
+print(" ")
+print(">>>>>>DARK CYBER WEAPON<<<<<<")
+print(" ")
+
+print("LOADING SYSTEM INFORMATION>>>")
+print(" ")
+print("###...........................15%")
+print(" ")
+print("##########....................40%")
+print(" ")
+print("#####################.........70%")
+print(" ")
+print("##########################....87%")
+print(" ") 
+print(" #############################100%")
+print(" ")
+print(" ")
+
+import platform
+
+print("$$___DARK CYBER WEAPON SYSTEM INFORMATION___$$")
+
+print(" ")
+print(" ")
+ 
+print("ANDROID SYSTEM")
+my_system = platform.uname()
+ 
+
+print(f"∆∆∆>> System: {my_system.system}")
+print("")
+print(f"∆∆∆>> Node Name: {my_system.node}")
+print("")
+print(f"∆∆∆>> Release: {my_system.release}")
+print("")
+print(f"∆∆∆>> Version: {my_system.version}")
+print("")
+print(f"∆∆∆>> Machine: {my_system.machine}")
+print("")
+print(f"∆∆∆>> Processor: {my_system.processor}") 
+print("")
+print(">>>>>FINISHED!!!!!!!!!!!!")
+print("")
+print(" press ctrl+z to stop and exit")
