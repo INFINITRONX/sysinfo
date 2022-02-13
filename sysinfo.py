b21a696476d36e173ec52e9b588633a1bd35ad6d
ASA264 <84116335+ASA264@users.noreply.github.com>
2022-02-13 19:41:42 +0530

    sysinfo.py

--- sysinfo.py
+++ sysinfo.py
@@ -1,74 +1,146 @@
-
-print(""" ______   ______ _____ _____ __  __
+import psutil
+import platform
+from datetime import datetime
+def get_size(bytes, suffix="B"):
+    """
+    Scale bytes to its proper format
+    e.g:
+        1253656 => '1.20MB'
+        1253656678 => '1.17GB'
+    """
+    factor = 1024
+    for unit in ["", "K", "M", "G", "T", "P"]:
+        if bytes < factor:
+            return f"{bytes:.2f}{unit}{suffix}"
+        bytes /= factor
+        
+print(""" ______   ______ _____ _____ __  __ 
 / ___\ \ / / ___|_   _| ____|  \/  |
 \___\ \ V /\___ \ | | |  _| | |\/| |
  ___) || |  ___) || | | |___| |  | |
 |____/ |_| |____/ |_| |_____|_|  |_|
-
- ___ _   _ _____ ___  ____  __  __    _  _____ ___ ___  _   _
+                                    
+ ___ _   _ _____ ___  ____  __  __    _  _____ ___ ___  _   _ 
 |_ _| \ | |  ___/ _ \|  _ \|  \/  |  / \|_   _|_ _/ _ \| \ | |
  | ||  \| | |_ | | | | |_) | |\/| | / _ \ | |  | | | | |  \| |
  | || |\  |  _|| |_| |  _ <| |  | |/ ___ \| |  | | |_| | |\  |
 |___|_| \_|_|   \___/|_| \_\_|  |_/_/   \_\_| |___\___/|_| \_|""")
+print("")
+print("                             <<<<<TOOL BY-:DARK_CYBER_WEAPON>>>>>") 
+print("")
+print("")
+
+print("|||||||||||||||||||LOADING|||||||||||||||||||||||")
+print("")
+uname = platform.uname()
+print(f"System: {uname.system}")
+print(f"Node Name: {uname.node}")
+print(f"Release: {uname.release}")
+print(f"Version: {uname.version}")
+print(f"Machine: {uname.machine}")
+print("")
+
+
+# Boot Time
+print("")
+print("<<<<<<<<<<<|||BOOT-TIME|||>>>>>>>>>>>")
+print("")
+boot_time_timestamp = psutil.boot_time()
+bt = datetime.fromtimestamp(boot_time_timestamp)
+print(f"Boot Time: {bt.year}/{bt.month}/{bt.day} {bt.hour}:{bt.minute}:{bt.second}")
+print("")
+# let's print CPU information
+print("")
+print("<<<<<<<<<<|||CPU-INFORMATION|||>>>>>>>>>>")
+print("")
+# number of cores
+print("Physical cores:", psutil.cpu_count(logical=False))
+print("Total cores:", psutil.cpu_count(logical=True))
+# CPU frequencies
+cpufreq = psutil.cpu_freq()
+print(f"Max Frequency: {cpufreq.max:.2f}Mhz")
+print(f"Min Frequency: {cpufreq.min:.2f}Mhz")
+print(f"Current Frequency: {cpufreq.current:.2f}Mhz")
+# CPU usage
+print("CPU Usage Per Core:")
+for i, percentage in enumerate(psutil.cpu_percent(percpu=True, interval=1)):
+    print(f"Core {i}: {percentage}%")
+print(f"Total CPU Usage: {psutil.cpu_percent()}%")
+
+# Memory Information
+print("")
+print("<<<<<<<|||MEMORY-INFORMATION|||>>>>>>>")
+print("")
+# get the memory details
+svmem = psutil.virtual_memory()
+print(f"Total: {get_size(svmem.total)}")
+print(f"Available: {get_size(svmem.available)}")
+print(f"Used: {get_size(svmem.used)}")
+print(f"Percentage: {svmem.percent}%")
 print(" ")
-print("                              >>>>>>DARK CYBER WEAPON<<<<<<")
+print("===swap===")
 print(" ")
-print("TODAY>>>   DATE AND TIME")
+# get the swap memory details (if exists)
+swap = psutil.swap_memory()
+print(f"Total: {get_size(swap.total)}")
+print(f"Free: {get_size(swap.free)}")
+print(f"Used: {get_size(swap.used)}")
+print(f"Percentage: {swap.percent}%")
+print("")
+# Disk Information
+print("<<<<<<<<<<|||DISK-INFORMATION|||>>>>>>>>>>")
+print("")
+print("Partitions and Usage:")
+# get all disk partitions
+partitions = psutil.disk_partitions()
+for partition in partitions:
+    print(f"=== Device: {partition.device} ===")
+    print(f"  Mountpoint: {partition.mountpoint}")
+    print(f"  File system type: {partition.fstype}")
+    try:
+        partition_usage = psutil.disk_usage(partition.mountpoint)
+    except PermissionError:
+        # this can be catched due to the disk that
+        # isn't ready
+        continue
+    print(f"  Total Size: {get_size(partition_usage.total)}")
+    print(f"  Used: {get_size(partition_usage.used)}")
+    print(f"  Free: {get_size(partition_usage.free)}")
+    print(f"  Percentage: {partition_usage.percent}%")
+# get IO statistics since boot
+disk_io = psutil.disk_io_counters()
+print("")
+print(f"Total read: {get_size(disk_io.read_bytes)}")
+print(f"Total write: {get_size(disk_io.write_bytes)}")
+
+# Network information
+print(" ")
+print("<<<<<<<<<<|||NETWORK-INFORMATION|||>>>>>>>>>>")
+print(" ")
+# get all network interfaces (virtual and physical)
+if_addrs = psutil.net_if_addrs()
+for interface_name, interface_addresses in if_addrs.items():
+    for address in interface_addresses:
+        print(f"=== Interface: {interface_name} ===")
+        if str(address.family) == 'AddressFamily.AF_INET':
+            print(f"  IP Address: {address.address}")
+            print(f"  Netmask: {address.netmask}")
+            print(f"  Broadcast IP: {address.broadcast}")
+        elif str(address.family) == 'AddressFamily.AF_PACKET':
+            print(f"  MAC Address: {address.address}")
+            print(f"  Netmask: {address.netmask}")
+            print(f"  Broadcast MAC: {address.broadcast}")
+# get IO statistics since boot
+net_io = psutil.net_io_counters()
+print(" ")
+print(f"Total Bytes Sent: {get_size(net_io.bytes_sent)}")
+print(f"Total Bytes Received: {get_size(net_io.bytes_recv)}")
 print(" ")
 
 
-import datetime
-e = datetime.datetime.now()
-print ("Current date and time = %s" % e)
-print("<<<DATE>>>")
-print("")
-print ("Today's date:  = %s/%s/%s" % (e.day, e.month, e.year))
-print("<<<TIME>>>")
-print("")
-print ("The time is now: = %s:%s:%s" % (e.hour, e.minute, e.second))
-print("")
-print("")
-print("^>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-print("")
+print(""" mmmmmm mmmmm  mm   m mmmmm   mmmm  m    m
+ #        #    #"m  #   #    #"   " #    #
+ #mmmmm   #    # #m #   #    "#mmm  #mmmm#
+ #        #    #  # #   #        "# #    #
+ #      mm#mm  #   ## mm#mm  "mmm#" #    #""")
 
-print("LOADING SYSTEM INFORMATION>>>")
-print(" ")
-print("###...........................15%")
-print(" ")
-print("##########....................40%")
-print(" ")
-print("#####################.........70%")
-print(" ")
-print("##########################....87%")
-print(" ") 
-print("##############################100%")
-print(" ")
-print(" ")
-
-import platform
-
-print("$$___DARK CYBER WEAPON SYSTEM INFORMATION___$$")
-
-print(" ")
-
- 
-print("ANDROID SYSTEM")
-print("")
-my_system = platform.uname()
- 
-
-print(f"∆∆∆>> System: {my_system.system}")
-print("")
-print(f"∆∆∆>> Node Name: {my_system.node}")
-print("")
-print(f"∆∆∆>> Release: {my_system.release}")
-print("")
-print(f"∆∆∆>> Version: {my_system.version}")
-print("")
-print(f"∆∆∆>> Machine: {my_system.machine}")
-print("")
-print(f"∆∆∆>> Processor: {my_system.processor}") 
-print("")
-print(">>>>>FINISHED!!!!!!!!!!!!")
-print("")
-print(" type exit to stop and exit")
